f3846483e2fdf0366e48fa91630b56d6403bcb60
Jason <j_bice@apple.com>
2020-09-25 09:25:21 -0400

    Project Setup

---
+++ FeelBetter/FeelBetter/FeelBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeelBetterApp.swift
+//  FeelBetter
+//
+//  Created by Jason Bice on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct FeelBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
